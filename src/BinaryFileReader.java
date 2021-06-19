6c9b1ae0b0ec20708847c387072173329ec24afc
Ryan McClue <re.mcclue@protonmail.com>
2021-06-19 10:28:31 +1000

    Generic byte reading

--- src/BinaryFileReader.java
+++ src/BinaryFileReader.java
@@ -5,7 +5,7 @@ public class
 BinaryFileReader
 {
   public static
-  byte[] read_entire_file(String file_name)
+  ByteArrayInputStream read_entire_file(String file_name)
   {
     InputStream file_stream = null;
     try 
@@ -28,34 +28,83 @@ BinaryFileReader
       Breakpoint.bp(e.getMessage());
     }
     
-    return file_data;
+    return new ByteArrayInputStream(file_data);
   }
 
-  public static long
-  read_long(ByteArrayInputStream stream, int offset)
+  public static boolean
+  areLittleEndian()
+  {
+    return ByteOrder.nativeOrder().equals(ByteOrder.LITTLE_ENDIAN);
+  }
+
+  private static ByteBuffer
+  read_bytes(ByteArrayInputStream stream, int offset, int num_bytes)
   {
     stream.reset();
-
-    byte[] byte_holder = new byte[Long.BYTES];
-
     stream.skip(offset);
-    int bytes_read = stream.read(byte_holder, 0, Long.BYTES);
+
+    byte[] byte_holder = new byte[num_bytes];
+    int bytes_read = stream.read(byte_holder, 0, byte_holder.length);
     if (bytes_read == 0 || bytes_read == -1)
     {
       Breakpoint.bp();
     }
 
-    long val = ByteBuffer.wrap(byte_holder).getLong();
+    return ByteBuffer.wrap(byte_holder);
+  }
 
-    stream.reset();
-
-    return val;
+  public static <T> T
+  read_val(ByteArrayInputStream stream, Class<T> type, int offset)
+  {
+    byte[] byte_holder = null;
+    if (type == Integer.class)
+    {
+      ByteBuffer val = read_bytes(stream, offset, Integer.BYTES);
+      return Integer.valueOf(val.getInt());
+    }
   }
 
   public static void
   main(String[] args)
   {
-    System.out.println("hi");
-    byte[] data = BinaryFileReader.read_entire_file("file.txt"); 
+    ByteArrayInputStream data = 
+      BinaryFileReader.read_entire_file("file.bin"); 
+    int num = 
+      BinaryFileReader.read_val(data, Integer.class, 0).intValue();
+  }
+}
+
+class
+LocalisationText
+{
+  String mo_data;
+  boolean reversed; // endianness
+  int num_strings;
+  int original_table_offset;
+  int translated_table_offset;
+  int hash_num_entries;
+  int hash_offset;
+
+  char hash_table; // is void*
+  boolean is_language_right_to_left;
+
+  public
+  LocalisationText(String folder, String language_name)
+  {
+    String name = String.format("%s/%s", folder, language_name);
+
+    ByteArrayInputStream data = 
+      BinaryFileReader.read_entire_file(name);
+
+    // this.hash_table = (void *)data;
+    int target_magic = 0x950412de;
+    int target_magic_reversed = 0xde120495;
+    //int magic = BinaryFileReader.read_val(data, Integer.class, 0);
+    //if (magic == target_magic)
+    //{
+
+    //}
+
+    data.close();
   }
 }
